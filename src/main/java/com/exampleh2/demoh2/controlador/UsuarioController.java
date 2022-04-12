92ef8631b9cec3393b176879d470109afa4d4935
george1002jlbb <george-1002@hotmail.com>
2022-04-12 11:36:27 -0500

    comentario

--- src/main/java/com/exampleh2/demoh2/controlador/UsuarioController.java
+++ src/main/java/com/exampleh2/demoh2/controlador/UsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
-
+    // clase controladora
     @Autowired
     private UsuarioServiceImp usuarioServiceImp;
 
